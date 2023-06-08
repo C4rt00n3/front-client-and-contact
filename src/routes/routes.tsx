89ab0c7af2f474a5aa9ee8f576337c057ba6b4ab
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-06-08 19:50:23 -0300

    fix/theme

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "../pages/login/index.login";
 import Register from "../pages/register/index.register";
-import Home from "../pages/home/index.home";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../context/authContext/index.context";
 import Protect from "./ProtectRoute/index.protect";
-import Loading from "../pages/Loading/index.load";
 
 const RoutesMain = () => {
-  const { token, get } = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
 
   return (
     <Routes>
